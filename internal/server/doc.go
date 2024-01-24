d4bc02eed0cb1b1ea2d37ea7ad7e5cfe74276f01
Zasoba <zasobav@c415mk394xl9jr2.detss.corpintra.net>
2024-01-24 21:45:25 +0100

    Add swagger doc src file

---
+++ internal/server/doc.go
@@ -0,0 +1,14 @@
+// Package server Vector Study Case API
+//
+//	Swagger specification for Vector study case
+//
+//	   Version: 0.0.1
+//	   Schemes: https, http
+//
+//	   Contact: Vladyslav Zasoba <vladzasoba16@gmail.com>
+//
+//	   Produces:
+//	   - text/plain
+//
+// swagger:meta
+package server
